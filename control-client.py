aef86728b57f63c6c141ff4bc2bb6cf0c1080bb3
Justin Bruce <justin@Justins-MacBook-Pro.local>
2018-10-25 19:37:40 -0500

    add functionality to controller-client and server file

--- control-client.py
+++ control-client.py
@@ -1,1 +1,54 @@
-#Client file
+#Client controller file
+from socket import *
+import sys
+
+arg_list = sys.argv
+if len(arg_list) != 4:
+    print('Usage: python3 control-client.py <server-ip> <server-port> <receiver-port>')
+
+server_ip = arg_list[1]
+server_port = int(arg_list[2])
+receiver_port = int(arg_list[3])
+
+print('Type SETUP to initiate a TCP connection with server.')
+print('After SETUP type PLAY or PAUSE at anytime.')
+
+user_cmd = input()
+user_cmd = user_cmd.upper()
+
+#gets what data the client wants from the server
+def get_sensor_data():
+    print('Input what reading you would like displayed.')
+    print('79 for Oxygen, 84 for temperature, 80 for pressure, or * for All')
+    print('example inputs: 79,84 or 80 or *')
+    sensor_data = input()
+    sensor_data_check = sensor_data.split(',')
+    for each in sensor_data_check:
+        if each == '79' or each == '84' or each == '80' or each == '*':
+            sensor_data_msg = sensor_data
+        else:
+            print('Invalid input')
+            sensor_data_msg =''
+    return sensor_data_msg
+
+
+if user_cmd == 'SETUP':
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((server_ip,server_port))
+    sensor_msg = get_sensor_data()
+    while sensor_msg == '':
+        sensor_msg = get_sensor_data()
+
+    setup_msg = 'SETUP rtsp://'+server_ip+'/RTSP/2.0 \nCSeq: 302 \nTransport: UDP;unicast;dest_addr":4588" \nSensor: '+sensor_msg
+    client_socket.send(setup_msg.encode())
+
+while True:
+    user_cmd = input()
+    user_cmd = user_cmd.upper()
+
+if user_cmd == 'PLAY':
+    print('play')
+elif user_cmd == 'PAUSE':
+    print('pause')
+else:
+    print('Unknow command')
--- server.py
+++ server.py
@@ -16,14 +16,16 @@ oxygen_file = arg_list[2]
 tempeture_file = arg_list[3]
 pressure_file = arg_list[4]
 
+#make sure all file are .bin files
 o_check = oxygen_file.split('.')
 t_check = tempeture_file.split('.')
 p_check = pressure_file.split('.')
 
-if o_check != 'bin' and t_check != 'bin' and p_check != 'bin':
+if o_check[1] != 'bin' and t_check[1] != 'bin' and p_check[1] != 'bin':
     print('Usage: python3 server.py <server port> <oxygen_file.bin> <tempeture_file.bin> <pressure_file.bin>')
     print('All files must be bin files')
     sys.exit(0)
+
 ########
 #  Used for creation of random bin file
 # directory = os.getcwd()
@@ -34,6 +36,7 @@ if o_check != 'bin' and t_check != 'bin' and p_check != 'bin':
 #     fout.write(os.urandom(2048)) 
 ########
 
+#reads data from all .bin file and converts them to integers
 def get_data():
     ######## why use 5 bits?
     #Oxygen bit collection
@@ -126,14 +129,36 @@ def get_data():
 
     return oxygen_level, temp_level, pressure_level  
 
-def clientthread():
-    while play == True:
-        levels = get_data()
-        #send over udp
-        while pause == True:
-            pass
+#reads in data from control client
+def clientthread(conn,port):
+    while True:
+        sentence = conn.recv(1024).decode()
+        if sentence.startswith('SETUP'):
+            sentence = sentence.split('\n')
+            setup_line = sentence[0]
+            seq_line = sentence[1]
+            transport_line = sentence[2]
+            sensor_line = sentence[3]
+            print(setup_line)
+            print('xxxx')
+            print(seq_line)
+            print('xxxx')
+            print(transport_line)
+            print('xxxx')
+            print(sensor_line)
+        if sentence.startswith('PLAY'):
+            print("play")
+        if sentence.startswith('PAUSE'):
+            print('pause')
+
+        # while play == True:
+        #     levels = get_data()
+        #     #send over udp
+        #     while pause == True:
+        #         pass
 
 
+#setup for TCP connection
 tcp = socket(AF_INET, SOCK_STREAM)
 tcp.bind(('',server_port))
 tcp.listen(5)
